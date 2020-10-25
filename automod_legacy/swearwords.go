87c7307477d4425309fe9733fcc0b7e91c54cbe2
ravery412 <73443456+ravery412@users.noreply.github.com>
2020-10-25 15:55:03 -0400

    Update swearwords.go

--- automod_legacy/swearwords.go
+++ automod_legacy/swearwords.go
@@ -1,78 +1,47 @@
 package automod_legacy
-
-// This list is in alphabethical order
 // Open a pr or bug me on discord if you want a word added
 var BuiltinSwearWords = map[string]bool{
-	"anal":        true,
-	"anus":        true,
-	"arse":        true,
-	"ass":         true,
-	"b1tch":       true,
-	"ballsack":    true,
-	"bastard":     true,
-	"bitch":       true,
-	"biatch":      true,
-	"blowjob":     true,
-	"bollock":     true,
-	"bollok":      true,
-	"boner":       true,
-	"boob":        true,
-	"boobs":       true,
-	"buttplug":    true,
-	"clitoris":    true,
-	"cock":        true,
-	"cum":         true,
-	"cunt":        true,
-	"dick":        true,
-	"dildo":       true,
-	"dyke":        true,
-	"erection":    true,
-	"fag":         true,
-	"faggot":      true,
-	"feck":        true,
-	"fellate":     true,
-	"fellatio":    true,
-	"felching":    true,
-	"fuck":        true,
-	"fucks":       true,
-	"fudgepacker": true,
-	"genitals":    true,
-	"hell":        true,
-	"jerk":        true,
-	"jizz":        true,
-	"knobend":     true,
-	"labia":       true,
-	"masturbate":  true,
-	"muff":        true,
-	"nigger":      true,
-	"nigga":       true,
-	"penis":       true,
-	"piss":        true,
-	"poop":        true,
-	"pube":        true,
-	"pussy":       true,
-	"queer":       true,
-	"scrotum":     true,
-	"sex":         true,
-	"shit":        true,
-	"sh1t":        true,
-	"slut":        true,
-	"smegma":      true,
-	"spunk":       true,
-	"tit":         true,
-	"tranny":       true,
-	"trannies":     true,
-	"tosser":      true,
-	"turd":        true,
-	"twat":        true,
-	"vagina":      true,
-	"wank":        true,
-	"whore":       true,
-	"tits":        true,
-	"titty":       true,
-	"asshole":     true,
-	"fvck":        true,
-	"asshat":      true,
-	"pu55y":       true,
-	"pen1s":       true,
-}
+	
+	nigga
+	nigger
+	n1gger
+	fuck ravery
+	cum
+	sex
+	s3x
+	ssex
+	sseexx
+	sexx
+	seexx
+	seex
+	s33x
+	boobs
+	dick
+	vagina
+	d1ck
+	vag1na
+	ravery sucks
+	go fuck yourself
+	kys
+	kill yourself
+	kill urself
+	kill your self
+	ravery is  a bitch
+	cunt
+	penis
+	p3n1s
+	p3nis
+	pen1s
+	sperm
+	sp3rm
+	vagin4
+	vagina
+	vag1n4
+	vag1na
+	chikabou sucks
+	ravery trash
+	this server is trash
+	trash server
+	chikabou trash
+	fuck chikabou
+	chikabou fuck
